42fdbe4c9617b29dd784c8a338987e7b68d198b4
andy2333 <1061501351@qq.com>
2021-01-15 01:33:38 -0800

    增加led_设备树驱动，未完成

---
+++ led_button/led_drv/led_drv.c
@@ -0,0 +1,90 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <asm/io.h>
+#include <asm/uaccess.h>
+
+#include <linux/cdev.h>
+#include <linux/device.h>
+
+static dev_t dev_num = 0;
+static struct cdev *cdevice = NULL;
+static struct class *sys_class = NULL;
+static struct device *class_device = NULL;
+
+static volatile unsigned int *gpio_data;
+
+int led_probe(struct platform_device *pdev)
+{
+
+	struct resource *res = NULL;
+	unsigned int led_pin;
+	res = platform_get_esource(pdev,IORESOURCE_MEM,0);
+	if(res){
+		led_pin = res->start;
+	}
+	else{
+		of_property_read_u8(pdev->dev.of_node,"reg",(u8 *)&led_pin);	
+	}
+	printk("led_pin = %d\n",led_pin);
+	if(!led_pin){
+		return -EINVAL;
+	}
+	gpio_data = (int *)ioremap(led_pin,1);
+
+	printk(KERN_INFO "led_probe found led\n");
+	cdev_init(cdevice,&led_fops);
+
+	alloc_chrdev_region(&dev_num, 0, 1, "yangbkLed");
+	cdev_add(cdevice,dev_num,1);
+
+	sys_class = class_create(THIS_MODULE, "yangbkLed");
+	class_device = device_create(sys_class, NULL, dec_num, NULL, "yangbkDevice");
+	return 0 ;
+	
+}
+
+
+int led_remove(struct platform_device *pdev)
+{
+	printk(KERN_INFO "led remove, remove led\n");
+
+	device_destroy(sys_class,dev_num);
+	class_destroy(sys_class);
+
+	unregister_chrdev_region(dev_num, 1);
+	cdev_del(cdevice);
+	iounmap(gpio_data);
+	return 0;
+}
+
+static struct of_device_id of_match_table={
+	.compatible = "ybk_led",
+	.data = NULL,
+};
+
+struct platform_driver  led_drv={
+	.probe = led_probe,
+	.remove = led_remove,
+	.driver = {
+		.name = "idle",
+		.of_match_table = &of_match_table,
+	}
+};
+
+static int __init led_drv_init(void)
+{
+	printk(KERN_INFO "led_drv init \n");
+	platform_driver_register(&led_drv);
+	return 0 ;
+}
+static void __exit led_drv_exit(void)
+{
+	printk(KERN_INFO "led_drv exit \n");
+	platform_driver_unregister(&led_drv);
+}
+
+module_init(led_drv_init);
+module_exit(led_drv_exit);
+
