26f4b5dc167899057a440ee673e41ef800d6c043
andy2333 <1061501351@qq.com>
2021-01-28 01:41:18 -0800

    temp

--- 04_led_platform_tree/led_drv_platform_tree.c
+++ 04_led_platform_tree/led_drv_platform_tree.c
@@ -6,12 +6,59 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
 
+struct gpio_led_data{
+	const char *desc;
+	int gpio;
+	int active_high;
+};
+
+struct gpio_leds_platform_data
+{
+	struct gpio_led_data *leds;
+	int nleds;
+};
+
+
+
 struct cdev *dt_test_dev;
 dev_t gpio_led_dev_t;
 struct class *myclass;
+struct gpio_leds_platform_data *pdata;
 
 
-struct 	gpio_leds_platform_data *pdata;
+static struct 	gpio_leds_platform_data *gpio_leds_get_devtree_pdata(struct device *dev)
+{
+	struct device_node *node,*pp;
+	struct gpio_leds_platform_data *pdata;
+	struct gpio_led_data * led;
+	int nleds;
+	int i;
+
+	node = dev->of_node;
+	if(!node)
+		return ERR_PTR(-ENODEV);
+
+	nleds = of_get_child_count(node);
+	if(nleds == 0)
+		return ERR_PTR(-ENODEV);
+
+	pdata = devm_kzalloc(dev,sizeof(*pdata) + nleds * sizeof(*led),GFP_KERNEL);
+	if(!pdata)
+		return ERR_PTR(-ENOMEM);
+	pdata->leds = (struct gpio_led_data *)(pdata + 1);
+	pdata->nleds = nleds;
+	i = 0;
+	for_each_child_of_node(node,pp)
+	{
+		enum of_gpio_flags flags;
+		led = (struct gpio_led_data *)&pdata->leds[i++];
+		led->desc = of_get_property(pp,"label", NULL);
+		led->gpio = of_get_gpio_flags(pp,0,&flags);
+		led->active_high = flags & OF_GPIO_ACTIVE_LOW;
+		
+	}
+	
+}
 
 
 static const struct of_device_id ptr[] = 
@@ -21,30 +68,32 @@ static const struct of_device_id ptr[] =
 };
 
 
-struct gpio_leds_platform_data{
-	struct gpio_led_data *leds;
-	int nleds;
-};
 
+static int gpio_leds_open(struct inode *inode,struct file *file)
+{
+	printk("<kernel   %s\n>",__FUNCTION__);
+	return 0;
+}
 
 
 static const struct file_operations gpio_led_fops = 
 {
 	.owner =THIS_MODULE,
-	.oen
+	.open = gpio_leds_open,
 };
+
 
 static int led_tree_proble(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct device *mydevice;
 	int ret;
-	pdata = gpio_led_get_devtree_pdata(dev);
+	pdata = gpio_leds_get_devtree_pdata(dev);
 	if(IS_ERR(pdata))
 		return PTR_ERR(pdata);
 	
 	dt_test_dev = cdev_alloc();
-	if(IS_ERR(dt_test_dev())
+	if(IS_ERR(dt_test_dev))
 		return PTR_ERR(dt_test_dev);
 	cdev_init(dt_test_dev, &gpio_led_fops);
 	ret = alloc_chrdev_region(gpio_led_dev_t,0,pdata->nleds,"gpio-leds");
@@ -70,6 +119,17 @@ fail2:
 fail3:
 	return -1;
 }
+
+static int led_tree_remove(struct platform_device *pdev)
+{
+	device_destroy(myclass,gpio_led_dev_t);
+	class_destroy(myclass);
+	cdev_del(dt_test_dev);
+	unregister_chrdev_region(gpio_led_dev_t,pdata->nleds);
+
+	return 0;
+}
+
 
 static struct platform_driver drv = 
 {
